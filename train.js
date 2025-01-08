b809780a8adc353319a9173809bed7c98f5af639
stanley <stanley.gonzales08@gmail.com>
2025-01-08 15:18:10 +0800

    Rock-Paper-Scissors

--- train.js
+++ train.js
@@ -1,18 +1,60 @@
-const akane_pic = document.getElementById('akane');
-const button = document.getElementById('begone')
+const choices = ['rock', 'paper', 'scissor'];
+const playerDisplay = document.getElementById('player-choice');
+const computerDisplay = document.getElementById('computer-choice');
+const results = document.getElementById('result');
+const playerScore = document.getElementById('player-score');
+const computerScore = document.getElementById('computer-score');
+let playerCounter = 0;
+let computerCounter = 0; 
 
-button.addEventListener('click', (event) => {
 
-    if(akane_pic.style.visibility === 'hidden'){
+function playGame(choice){
 
-        akane_pic.style.visibility = 'visible';
-        button.textContent = 'Hide';
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = ``
+    console.log(computerChoice)
+
+    if(choice === computerChoice){
+
+        result = `IT'S A TIE!`        
 
     }else{
 
-        akane_pic.style.visibility = 'hidden';
-        button.textContent = 'show'
-    
+        switch(choice){
+            case 'rock':
+                result = (computerChoice === 'scissors')  ? 'YOU WIN' : 'YOU LOSE'
+                break;
+            case 'paper':
+                result = (computerChoice === 'rock')  ? 'YOU WIN' : 'YOU LOSE'
+                break;
+            case 'scissor':
+                result = (computerChoice === 'paper')  ? 'YOU WIN' : 'YOU LOSE'
+                break;
+
+        }
     }
 
-});
+    playerDisplay.textContent = `PLAYER: ${choice}`
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`
+    results.textContent = result;
+
+    results.classList.remove('correct', 'wrong');
+
+    switch(result){
+
+        case 'YOU WIN':
+            results.classList.add('correct');
+            playerCounter++;
+            playerScore.textContent = playerCounter
+            break;
+        case 'YOU LOSE':
+            results.classList.add('wrong');
+            computerCounter++;
+            computerScore.textContent = computerCounter
+            break;
+
+    }
+
+
+
+}
