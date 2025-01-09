b01787c6e16d94c4bf31aebdcb4601a40ba7b014
stanley <stanley.gonzales08@gmail.com>
2025-01-09 22:18:36 +0800

    Promises

--- train.js
+++ train.js
@@ -1,54 +1,50 @@
-const slides = document.querySelectorAll('.slides img');
-let slideIndex = 0;
-let intervalID = null;
+function walkDog(){
 
-document.addEventListener('DOMContentLoaded', init_slider);
-
-function init_slider(){
-
-    if(slides.length > 0){
-
-        slides[slideIndex].classList.add("displaySlide");
-        intervalID  = setInterval(nextSlide,5000);
-
-    }
-
-}
-
-function showSlide(index){
-
-    if(index >= slides.length){
-
-        slideIndex = 0;
-
-    }else if(index < 0){
-
-        slideIndex = slides.length - 1;
-
-    }
-
-    slides.forEach(slide => {
-
-        slide.classList.remove('displaySlide');
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dogWalked = false;
+            if(dogWalked){
+                resolve('You walk the dog 🐶');
+            }else{
+                reject('You did not walk dogg')
+            }
+        }, 1500);
     })
 
-    slides[slideIndex].classList.add('displaySlide');
+}
 
+function cleanKitchen(){
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const cleanedKitchen = true;
+            if(cleanKitchen){
+                resolve('You cleaned the kitchen 🧹');
+            }else{
+                reject('You did not clean kitchen')
+            }
+        }, 2500);
+    })
 
 }
 
-function nextSlide(){
+function takeOutTrash(){
 
-    slideIndex++;
-    showSlide(slideIndex)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const takeTrashOutside = true;
+            if(takeTrashOutside){
+                resolve('You taked out the trash 🗑️');
+            }else{
+                reject('You did not take trash outside')
+            }
+        }, 500);
+    })
 
 }
 
-function prevSlide(){
-
-    clearInterval(intervalID)
-    slideIndex--;
-    showSlide(slideIndex)
-
-}
+walkDog().then(value => {console.log(value); return cleanKitchen()})
+         .then(value => {console.log(value); return takeOutTrash()})
+         .then(value => {console.log(value); console.log('Nice')})
+         .catch(error => console.error(error))
