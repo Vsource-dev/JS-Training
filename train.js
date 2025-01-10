f3e525643da7e5dee6a53392b7b28c20b13d294f
stanley <stanley.gonzales08@gmail.com>
2025-01-10 14:51:08 +0800

    pokemon

--- train.js
+++ train.js
@@ -1,84 +1,31 @@
-// function walkDog(){
+fetchData()
 
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             const dogWalked = false;
-//             if(dogWalked){
-//                 resolve('You walk the dog 🐶');
-//             }else{
-//                 reject('You did not walk dog')
-//             }
-//         }, 1500);
-//     })
-
-// }
-
-// function cleanKitchen(){
-
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             const cleanedKitchen = true;
-//             if(cleanKitchen){
-//                 resolve('You cleaned the kitchen 🧹');
-//             }else{
-//                 reject('You did not clean kitchen')
-//             }
-//         }, 2500);
-//     })
-
-// }
-
-// function takeOutTrash(){
-
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-
-//             const takeTrashOutside = true;
-//             if(takeTrashOutside){
-//                 resolve('You taked out the trash 🗑️');
-//             }else{
-//                 reject('You did not take trash outside')
-//             }
-//         }, 500);
-//     })
-
-// }
-
-// async function doChores(){
-
-//     try{
-
-//         const walkDogResult = await walkDog();
-//         console.log(walkDogResult);
+async function fetchData() {
     
-//         const cleanKitchenResult = await cleanKitchen();
-//         console.log(cleanKitchenResult);
-    
-//         const takeOutTrashResult = await takeOutTrash();
-//         console.log(takeOutTrashResult)
-    
-//         console.log('Finished All')
+    try{
 
-//     }catch(error){
+        const pokemonName = document.getElementById('pkmn').value;
 
-//         console.log(error)
-        
-//     }
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
 
-// }
+        if(!response.ok){
 
-// doChores();
+            throw new Error('No pokemon exist')
 
-const names = `["Spongebob", "Patrick", "Squidward", "Sandy"]`
+        }
+       
+        const data = await response.json()
+        const pokemonSprite = data.sprites.front_default;
+        const img = document.getElementById('pkmn-sprite');
 
-const person = `{"name": "Spongebob", "age" : 30, "IsEmployed": true,"Hobby": ["Fishing", "Driving", "Talking"]}`
+        img.src = pokemonSprite;
+        img.style.display = 'block';
+         
 
-const people = `[{"name": "Spongebob","age" : 30,"IsEmployed": true},
-                {"name": "Patrick","age" : 23,"IsEmployed": false},
-                {"name": "Squidward","age" : 25,"IsEmployed": true},
-                {"name": "Sandy","age" : 29,"IsEmployed": true}]`
+    }catch(error){
 
+        console.error(error)
 
-fetch("people.json").then(response => response.json())
-                    .then(value => value.forEach(val => console.log(val.age)))
-                    .catch(error => console.error(error));
+    }
+
+}
